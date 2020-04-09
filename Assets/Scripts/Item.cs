50facecfd324e7d97065bf44361a31194acc2b69
edxas <edxas@users.noreply.github.com>
2020-04-09 13:32:42 +0300

    prideta galimybe visose scenes uzsidet Items galimus

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Item : MonoBehaviour
+{
+    public int ID;
+    public string type;
+    public string description;
+    public Sprite icon;
+    public bool pickedUp;
+
+    [HideInInspector]
+    public bool equipped;
+
+    [HideInInspector]
+    public GameObject armour;
+
+    [HideInInspector]
+    public GameObject armourManager;
+
+    public bool playerArmour;
+
+    public void Start()
+    {
+        armourManager = GameObject.FindWithTag("ArmourManager");
+        //healthbar = GameObject.fin
+        if(!playerArmour)
+        {
+            int allArmour = armourManager.transform.childCount;
+            for (int i = 0; i < allArmour; i++)
+            {
+                if(armourManager.transform.GetChild(i).gameObject.GetComponent<Item>().ID == ID)
+                {
+                    armour = armourManager.transform.GetChild(i).gameObject;
+                }
+            }
+        }
+    }
+
+    public void Update()
+    {
+        if(equipped)
+        {
+            //perform
+            if (Input.GetKeyDown(KeyCode.O))
+                equipped = false;
+            if (equipped == false)
+                this.gameObject.SetActive(false);
+        }
+    }
+    public void ItemUsage()
+    {
+        //weapon
+        if(type == "Weapon")
+        {
+            equipped = true;
+        }
+
+        //armor
+        if (type == "Armour")
+        {
+            armour.SetActive(true);
+            armour.GetComponent<Item>().equipped = true;
+        }
+        //consumables
+        if (type == "Potion")
+        {
+           // 
+        }
+    }
+}
