885b84d1186748c293fff6df23b30e0da5cce01e
MindeOverMatter <mindaugas.drapanauskas@gmail.com>
2020-04-19 21:36:54 +0300

    Main menu v1

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class LoadOnClick : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void LoadScene(int level)
     {
         Application.LoadLevel(level);
