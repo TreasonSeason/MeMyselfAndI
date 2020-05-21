06ca325cc23b481ba587f2fff242aebe30d94f41
MelAaron <61549963+MelAaron@users.noreply.github.com>
2020-05-21 13:11:58 +0300

    Multipliers

---
+++ Assets/Scripts/MultiplierStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplierStats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Attack attacks;
+    public float damageMultiplier = 1f;
+    public float resistenceMultiplier = 1f;
+    //void Start()
+    //{
+    //    attacks.GetComponent<Attack>();
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
